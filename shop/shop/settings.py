8b32c793728b3744d191ca809d23aacc539199cd
vlad3216 <vladsavin29@gmail.com>
2022-10-08 16:37:54 +0300

    fix flake8

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vh6e)=ic8g=*eky)boscr+ifbq&69a&-t(9=adin9n+z-l+1$z'
+SECRET_KEY = 'django-insecure-vh6e)=ic8g=*eky)boscr+ifbq&69a&-t(9=adin9n+z-l+1$z' # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
